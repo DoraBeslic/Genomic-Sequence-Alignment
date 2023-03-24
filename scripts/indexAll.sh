004865da76404697947738090f5d2693912ec25a
DoraBeslic <beslic.i@northeastern.edu>
2023-03-24 19:22:39 -0400

    added scipt to index all samples

---
+++ scripts/indexAll.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+# indexAll.sh
+
+# Usage: bash scripts/indexAll.sh 1>results/logs/indexAll.log 2>results/logs/indexAll.err &
+
+bamOutPath="results/bam/"
+suffix=".bam"
+for inFile in $bamOutPath*$suffix
+do
+    # Remove the path from the filename and assign to pathRemoved
+    pathRemoved="${inFile/$bamOutPath/}"
+    # Remove the suffix from $pathRemoved and assign to sampleName
+    sampleName="${pathRemoved/$suffix/}"
+
+    samtools index $bamOutPath$sampleName$suffix
+    1>$bamOutPath$sampleName.bai
